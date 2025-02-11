7db4ded23b8d0a119b3972b3d18378a279f4fa6d
Gensgenerators <mustafafarah0514@gmail.com>
2025-02-11 17:36:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,20 +2,17 @@ function getPasswordStrength(password) {
     let strength = 'Very Weak';
     let color = 'red';
     let percentage = 20;
-
     const hasLowerCase = /[a-z]/.test(password);
     const hasUpperCase = /[A-Z]/.test(password);
     const hasNumbers = /[0-9]/.test(password);
     const hasSpecial = /[!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]/.test(password);
     const lengthCriteria = password.length >= 8;
-
     const criteriaCount =
         (hasLowerCase ? 1 : 0) +
         (hasUpperCase ? 1 : 0) +
         (hasNumbers ? 1 : 0) +
         (hasSpecial ? 1 : 0) +
         (lengthCriteria ? 1 : 0);
-
     switch (criteriaCount) {
         case 0:
         case 1:
@@ -57,26 +54,21 @@ document.getElementById('generate').addEventListener('click', function () {
     const includeSpecial = document.getElementById('includeSpecial').checked;
     const includeUppercase = document.getElementById('includeUppercase').checked;
     const includeLowercase = document.getElementById('includeLowercase').checked;
-
     let characters = '';
     if (includeLowercase) characters += 'abcdefghijklmnopqrstuvwxyz';
     if (includeUppercase) characters += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     if (includeNumbers) characters += '0123456789';
     if (includeSpecial) characters += '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
-
     if (!characters) {
         alert('Please select at least one character type.');
         return;
     }
-
     let password = '';
     for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * characters.length);
         password += characters[randomIndex];
     }
-
     document.getElementById('password').value = password;
-
     const { strength, color, percentage } = getPasswordStrength(password);
     document.getElementById('progressFill').style.width = `${percentage}%`;
     document.getElementById('progressFill').style.backgroundColor = color;
